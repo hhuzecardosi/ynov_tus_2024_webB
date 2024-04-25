4c4f5d41bcd3ab88304cb80abbf4af14e06c4d93
Hugo Huzé--Cardosi <hugo.cardosi@hotmail.fr>
2024-04-25 15:54:24 +0200

    test: add a few test to kill mutants in  receipt printer

--- ts/src/03-supermarket/tests/receipt-printer.spec.ts
+++ ts/src/03-supermarket/tests/receipt-printer.spec.ts
@@ -2,6 +2,7 @@ import {ReceiptPrinter} from "../src/ReceiptPrinter";
 import {Receipt} from "../src/model/Receipt";
 import {Product} from "../src/model/Product";
 import {ProductUnit} from "../src/model/ProductUnit";
+import {Discount} from "../src/model/Discount";
 
 class ReceiptParser{
 
@@ -47,5 +48,33 @@ describe('ReceiptPrinter', () => {
         expect(result).toBe("apples                              3.20\n"+
         "  1.60 * 2.000\n\n"+
         "Total:                              3.20")
+    });
+
+    it('should work with two items', () => {
+        // ARRANGE
+        const receipt = new Receipt();
+        receipt.addProduct(new Product('apples', ProductUnit.Kilo), 2, 1.6, 2 * 1.6);
+        receipt.addProduct(new Product('bananas', ProductUnit.Kilo), 1, 1.2, 1.2);
+
+        // ACT
+        const result = receiptPrinter.printReceipt(receipt);
+
+        // ASSERT
+        expect(result).toMatchSnapshot();
+    });
+
+    it('should work with two items and discounts on one', () => {
+        // ARRANGE
+        const apples = new Product('apples', ProductUnit.Kilo);
+        const receipt = new Receipt();
+        receipt.addProduct(apples, 2, 1.6, 2 * 1.6);
+        receipt.addProduct(new Product('bananas', ProductUnit.Kilo), 1, 1.2, 1.2);
+        receipt.addDiscount(new Discount(apples, '2 for 1', 1.6));
+
+        // ACT
+        const result = receiptPrinter.printReceipt(receipt);
+
+        // ASSERT
+        expect(result).toMatchSnapshot();
     });
 });
