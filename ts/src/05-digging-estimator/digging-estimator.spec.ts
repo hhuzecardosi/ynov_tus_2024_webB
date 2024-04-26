fb9148795b41684f24083850b16075734b0573e2
fhemery <f.hemery@hemit.fr>
2024-04-26 09:50:15 +0200

    Digging estimator with overload

---
+++ ts/src/05-digging-estimator/digging-estimator.spec.ts
@@ -0,0 +1,35 @@
+import {DiggingEstimator, TunnelTooLongForDelayException} from "./digging-estimator";
+
+class DiggingEstimatorTestOverload extends DiggingEstimator {
+  override get (rockType: string) : number[] {
+    return [0, 3, 5.5, 7];
+  }
+}
+
+describe("digging estimator", () => {
+
+  it("should return as Dr Pockovsky said", () => {
+    // To have it work, you need to go set the rates to [0, 3, 5.5, 7]
+    const estimator = new DiggingEstimatorTestOverload();
+
+    const result = estimator.tunnel(28, 2, "granite");
+
+    expect(result.total).toBe(48);
+  });
+
+  it("should return an error if tunnel cannot be dug because too long", () => {
+    const estimator = new DiggingEstimatorTestOverload();
+
+   expect(() => estimator.tunnel(36, 2, "granite")).toThrow(new TunnelTooLongForDelayException());
+
+  });
+
+  it("should return as Dr Pockovsky said for 1m tunnel", () => {
+    // To have it work, you need to go set the rates to [0, 3, 5.5, 7]
+    const estimator = new DiggingEstimatorTestOverload();
+
+    const result = estimator.tunnel(1, 2, "granite");
+
+    expect(result.total).toBe(0);
+  });
+});
